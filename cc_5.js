eaee41f09a942e79e2c6ce89485951976979481e
Logan Pfeiffer <Lpfeiffer48@usf.edu>
2026-02-22 21:31:12 -0500

    Created our Test population, and our functions to calculate base pay and over time

--- cc_5.js
+++ cc_5.js
@@ -1,1 +1,29 @@
-console.log("hello there")
+// Our Tropical island workers 
+
+let employees = [
+    { name: "Jeffery Epstein", hoursWorked: 75, hourlyRate: 450 },
+    { name: "Ghislaine Maxwell", hoursWorked: 80, hourlyRate: 400 },
+    { name: "Bill Gates", hoursWorked: 35, hourlyRate: 500 },
+    { name: "Elon Musk", hoursWorked: 45, hourlyRate: 600 },
+    { name: "Bill Clinton", hoursWorked: 40, hourlyRate: 175 }
+];
+
+function calculateBasePay( rate, hours) {
+// Calculate the base pay for an employee without overtime
+    if (hours > 40) {
+        return 40 * rate;
+    } else {
+        return hours * rate;
+    }
+}
+
+// Calculate the overtime pay for an employee
+function calculateOvertimePay(rate, hours) {
+    if (hours > 40) {
+       let overtimeHours = hours - 40;
+       let overtimeRate = rate * 1.5;
+       return overtimeHours * overtimeRate;
+    } else {
+       return 0;
+    }
+}
